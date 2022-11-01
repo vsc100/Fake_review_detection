c128ff4cba1f92b768a50f12c749a700f0e079c6
vsc100 <vaibhav.vg12@gmail.com>
2022-11-01 11:30:22 +0530

    initi~al commit

--- Deployment.py
+++ Deployment.py
@@ -67,7 +67,7 @@ def main():
     links = st.expander("Related Links")
     if links:
         links.write("[Dataset utilized](https://www.kaggle.com/akudnaver/amazon-reviews-dataset)")
-        links.write("[Github](https://github.com/vsc100/Fraud-Detection-in-Online-Consumer-Reviews-Using-Machine-Learning-Techniques)")
+        links.write("[Github](https://github.com/vsc100/Fake_review_detection)")
         
     # --CHECKBOXES--
     st.subheader("Information on the Classifier")
